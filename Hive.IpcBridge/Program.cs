952e2bc97047ffe587da595734d51ad94e1be006
Julian Kwieciński <julkwiec@gmail.com>
2016-09-10 02:07:11 +0200

    Poprawki

--- Hive.IpcBridge/Program.cs
+++ Hive.IpcBridge/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Threading.Tasks;
 
 namespace Hive.IpcServer
 {
@@ -8,8 +9,33 @@ namespace Hive.IpcServer
         [STAThread]
         public static void Main(string[] args)
         {
-            if (args.Length != 1) throw new ArgumentException("Invalid args");
-            new HiveServer().Run(Int32.Parse(args[0]));
+            try
+            {
+                if (args.Length < 1) throw new ArgumentException("Invalid args");
+                TryMonitorParent(args);
+                new HiveServer().Run(Int32.Parse(args[0]));
+            }
+            catch
+            {
+                Process.GetCurrentProcess().Kill();
+            }
+        }
+
+        private static void TryMonitorParent(string[] args)
+        {
+            if (args.Length >= 2)
+            {
+                var parent_proc = Process.GetProcessById(Int32.Parse(args[1]));
+                var this_proc = Process.GetCurrentProcess();
+                Task.Run(() =>
+                {
+                    while (!parent_proc.HasExited)
+                    {
+                        Task.Delay(100);
+                    }
+                    this_proc.Kill();
+                });
+            }
         }
     }
 }
