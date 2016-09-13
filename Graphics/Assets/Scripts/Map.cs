f997027adf17dcdd656adfffc0400137d17359ad
Julian Kwieciński <julkwiec@gmail.com>
2016-09-13 23:09:29 +0200

    Dodaję grafikę cz.2

---
+++ Graphics/Assets/Scripts/Map.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+using Hive.IpcClient;
+using System;
+
+public class Map : MonoBehaviour {
+
+	public GameObject hexPrefab;
+
+	// Size of the map in terms of number of hex tiles
+	// This is NOT representative of the amount of 
+	// world space that we're going to take up.
+	// (i.e. our tiles might be more or less than 1 Unity World Unit)
+	int width = 21;
+	int height = 21;
+
+	float xOffset = 0.882f;
+	float zOffset = 0.764f;
+
+	// Use this for initialization
+	void Start () {
+
+        int b = 5;
+
+		for (int x = 0, xbis = -10; x < width; x++, xbis++) {
+         
+			for (int y = 0, z = 10; y < height; y++, z--) {
+                
+                float xPos = x * xOffset;
+
+				// Are we on an odd row?
+				if( y % 2 == 1 ) {
+					xPos += xOffset/2f;
+				}
+
+				GameObject hex_go = (GameObject)Instantiate(hexPrefab, new Vector3( xPos,0, y * zOffset  ), Quaternion.identity  );
+
+				// Name the gameobject something sensible.
+				hex_go.name = "Hex_" + x + "_" + y;
+
+				// Make sure the hex is aware of its place on the map
+				hex_go.GetComponent<Hex>().x = x;
+				hex_go.GetComponent<Hex>().y = y;
+                hex_go.GetComponent<Hex>().c = z;
+                hex_go.GetComponent<Hex>().a = xbis - (z - (z & 1)) / 2;
+                hex_go.GetComponent<Hex>().b = -(hex_go.GetComponent<Hex>().a) - (hex_go.GetComponent<Hex>().c);
+
+
+                // if (y % 2 == 0)
+                //{
+                //   b++;
+                //}
+
+                // For a cleaner hierachy, parent this hex to the map
+                hex_go.transform.SetParent(this.transform);
+
+				// TODO: Quill needs to explain different optimization later...
+				hex_go.isStatic = true;
+
+			}
+           
+		}
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
---
+++ Graphics/Assets/Unit.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+using Hive.Common;
+
+public class Unit : MonoBehaviour {
+
+	public Vector3 destination;
+
+	float speed = 3;
+
+    public BugType bug;
+    public PlayerColor color;
+
+	// Use this for initialization
+	void Start () {
+		destination = transform.position;
+        //Debug.Log("name: " + name);
+
+        int position = name.IndexOf('_');
+        string bugName = name.Substring(0, position);
+
+        if (name.Contains("light"))
+            color = PlayerColor.White;
+        else if (name.Contains("dark"))
+            color = PlayerColor.Black;
+
+        switch (bugName)
+        {
+            case "QueenBee":
+                bug = BugType.QueenBee;
+                break;
+            case "Grasshopper":
+                bug = BugType.Grasshopper;
+                break;
+            case "Ant":
+                bug = BugType.SoldierAnt;
+                break;
+            case "Spider":
+                bug = BugType.Spider;
+                break;
+            case "Beetle":
+                bug = BugType.Beetle;
+                break;
+            case "Ladybird":
+                bug = BugType.Ladybug;
+                break;
+            case "Millipede":
+                bug = BugType.PillBug;
+                break;
+            case "Mosquito":
+                bug = BugType.Mosquito;
+                break;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        // Move towards our destination
+
+        // NOTE!  This just moves directly there, but really you'd want to feed
+        // this into a pathfinding system to get a list of sub-moves or something
+        // to walk a reasonable route.
+        // To see how to do this, look up my TILEMAP tutorial.  It does A* pathfinding
+        // and throughout the video I explain how you can apply that pathfinding
+        // to hexes.
+
+        //if (destination != transform.position)
+        //Debug.Log("dest: " + destination + ", pos: " + transform.position);
+
+        Vector3 dir = destination - transform.position;
+		Vector3 velocity = dir.normalized * speed * Time.deltaTime;
+
+		// Make sure the velocity doesn't actually exceed the distance we want.
+		velocity = Vector3.ClampMagnitude( velocity, dir.magnitude );
+
+		transform.Translate(velocity);
+
+	}
+
+	void NextTurn() {
+		// Set "destination" to be the position of the next tile
+		// in our pathfinding queue.
+	}
+}
